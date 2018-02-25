b1c50b2acb9f9f6ed54520528d508889cbdde2a4
Alex Nalivayko <a.nalivayko@zarplata.ru>
2018-02-25 20:40:32 +0700

    minnor fix

--- src/views/dislikes/index.js
+++ src/views/dislikes/index.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { isEmpty } from "lodash";
 
 export default () => {
   const dislikes = JSON.parse(localStorage.getItem("dislikes"));
+  const check = isEmpty(dislikes);
 
   return (
     <div>
-      <h3>Тут список Дизлайков</h3>
-      {dislikes.map(item =>
-        item.map(item => (
-          <ul key={item.id}>
-            <li>{item.name}</li>
-          </ul>
-        ))
-      )}
+      {check && <h3> Вы ничего не отметили </h3>}
+      {!check && <h3>Список не понравившихся вакансий</h3> &&
+        dislikes.map(item =>
+          item.map(item => (
+            <ul key={item.id}>
+              <li>{item.name}</li>
+            </ul>
+          ))
+        )}
     </div>
   );
 };
--- src/views/likes/index.js
+++ src/views/likes/index.js
@@ -1,18 +1,21 @@
 import React from "react";
+import { isEmpty } from "lodash";
 
 export default () => {
   const likes = JSON.parse(localStorage.getItem("likes"));
+  const check = isEmpty(likes);
 
   return (
     <div>
-      <h3>Тут список Лайков</h3>
-      {likes.map(item =>
-        item.map(item => (
-          <ul key={item.id}>
-            <li>{item.name}</li>
-          </ul>
-        ))
-      )}
+      {check && <h3> Вы ничего не отметили </h3>}
+      {!check &&
+        likes.map(item =>
+          item.map(item => (
+            <ul key={item.id}>
+              <li>{item.name}</li>
+            </ul>
+          ))
+        )}
     </div>
   );
 };
